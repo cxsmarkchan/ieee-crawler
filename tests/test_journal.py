a0e22943d6111f6d48c2caff11754e3aec5c6485
cxsmarkchan <cxsmarkchan@163.com>
2016-05-14 09:21:58 +0800

    add test to journal.py, with some small changes in journal.py

---
+++ tests/test_journal.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from app.ieee.journal import JournalCrawler
+
+
+class TestJournalCrawler(TestCase):
+    def setUp(self):
+        # IEEE Transactions on Smart Grid
+        self.__crawler = JournalCrawler(5165411)
+
+    def test_get_article(self):
+        url = 'http://ieeexplore.ieee.org/xpl/tocresult.jsp'
+        issue_number = 7361791
+        numbers = self.__crawler.get_article_numbers(
+            url=url,
+            issue_number=issue_number
+        )
+        self.assertEqual(
+            numbers,
+            [
+                '7361793', '7361800', '7270336', '7307227', '7271086',
+                '7323868', '7128404', '7061965', '7095603', '7112181',
+                '7095589', '7055343', '7152979', '7100914', '7064776',
+                '7055363', '7063233', '7175042', '7122362', '7115170',
+                '7063969', '7115166', '7121016', '7108042', '7273948',
+                '7163618', '7052382', '7172536', '7152969', '7091026',
+                '7127028', '7112542', '7127033', '7110380', '7173055',
+                '7244250', '7064771', '7130652', '7161386', '7361794',
+                '7361792', '7361698', '7017584', '6987318', '6998860',
+                '6999960', '7063250', '7017597', '7056506', '6920036',
+                '7045540', '7021935', '7083734', '7029711', '7047886',
+                '7063236', '7322272', '7293228', '7361697', '7361795',
+                '7361798', '7361801'
+            ]
+        )
